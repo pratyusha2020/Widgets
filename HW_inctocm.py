38a8a56550e708aeefd45bb097979a5aa19385d8
pratyusha2020 <92961548+pratyusha2020@users.noreply.github.com>
2025-08-24 14:37:51 -0700

    Add files via upload

---
+++ HW_inctocm.py
@@ -0,0 +1,30 @@
+import tkinter as tk
+
+def calculate_product():
+    try:
+        num1 = float(entry1.get())
+        
+        product = num1 * 2.54
+        result_label.config(text=f" {product}"+"cm")
+    except ValueError:
+        result_label.config(text="Error: Invalid input. Please enter numbers.")
+
+root = tk.Tk()
+root.title("Length Converter App")
+
+lbl= tk.Label(text= "Welcome to the length calculator app, this program converts lengths: in to cm!")
+lbl.pack()
+label1 = tk.Label(root, text="Enter your value in inches:",bg= "#e01868", fg= 'white')
+label1.pack()
+entry1 = tk.Entry(root)
+entry1.pack()
+
+
+
+calculate_button = tk.Button(root, text="Calculate", command=calculate_product)
+calculate_button.pack()
+
+result_label = tk.Label()
+result_label.pack()
+
+root.mainloop()
